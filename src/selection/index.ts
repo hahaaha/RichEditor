11deea8057d41e60c18ff0a532216d08b81434fc
haha <390519764@qq.com>
2020-09-24 13:36:27 +0800

    chore: add comment

--- src/selection/index.ts
+++ src/selection/index.ts
@@ -3,10 +3,12 @@ class selection {
     constructor() {
     }
 
+    // 存储选区
     static saveSelection() {
         let range = window.getSelection().getRangeAt(0)
         Global.range = range
     }
+    // 恢复选区
     static recoverSelection() {
         let selection = window.getSelection()
         selection.removeAllRanges()
